01ae357b4af5207a880a1b9df6bcf65a5b0ff3d2
Feral.Codes <feral.codes@gmail.com>
2024-10-24 11:53:19 -0700

    Started Es1

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,21 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+const filipis = {
+   name: 'Filipis',
+   cuisines: ['Italian'],
+   numberOfStars: 5,
+   favorited: true,
+};
+
+filipis.address = 'Temecula, Ca';
+filipis.zipcode = 92584;
+filipis.acceptsReservations = false;
+
+filipis.numberOfStars = 6;
+filipis.favorited = false;
+filipis.cuisines.push('American');
+
+function retrieveProperty(obj, key) {
+   
+}
